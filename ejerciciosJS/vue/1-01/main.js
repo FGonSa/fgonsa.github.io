11db7c18274fb77affb22fd17f28b1ff426db8bb
Francisco Javier González <78041280+FGonSa@users.noreply.github.com>
2022-01-29 17:06:34 +0100

    Create main.js

---
+++ ejerciciosJS/vue/1-01/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Calcetines',
+            image: './assets/images/socks_green.jpg',
+            description: "A pair of warm, fuzzy socks."
+        }
+    }
+})
