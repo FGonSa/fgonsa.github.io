0058d39f1861f73e3bef0b150233e1c49a469511
Francisco Javier González <78041280+FGonSa@users.noreply.github.com>
2022-01-30 12:37:48 +0100

    Add files via upload

---
+++ ejerciciosJS/vue/2-01/main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Calcetines',
+            image: './assets/images/socks_green.jpg',
+            description: "A pair of warm, fuzzy socks.",
+            url: 'http://www.google.com'
+        }
+    }
+})
