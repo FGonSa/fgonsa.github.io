228c0ec3cde662294207d04e20d4d8f989505f51
Francisco Javier González <fransecre@gmail.com>
2022-02-06 21:31:10 +0100

    Ejercicio Clases y Estilos

---
+++ ejerciciosJS/vue/4-01/main.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cesta: 0,
+            product: 'Calcetines',
+            image: './assets/images/socks_green.jpg',
+            description: "Par de calcetines suavecicos",
+            url: 'http://www.google.com',
+            stock: false,
+            details: ['talla S', 'talla M', 'talla L', 'talla XL']
+        }
+    },
+    methods: {
+        addCesta() {
+            this.cesta += 1
+        },
+        restarCesta() {
+            if (this.cesta > 0) {
+                this.cesta -= 1
+            }
+        },
+        cambiarStock() {
+            if (this.stock) {
+                this.stock = false
+            } else {
+                this.stock = true
+            }
+        }
+    }
+})
